08290a610bf816bf18a0db85a83e027c7e38ef44
Lobanova Viktoria <va_klevtsova@mail.ru>
2022-11-07 14:09:07 +0300

    Создано приложение-счётчик, в котором одна большая кнопка. Сколько раз на неё нажали — такая цифра и отображается на счётчике.

--- LobanovaViktoriaHomeWork_3_Sprint/ViewController.swift
+++ LobanovaViktoriaHomeWork_3_Sprint/ViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pressButton: UIButton!
+    
+    @IBOutlet weak var countLabel: UILabel!
+   
+    private var count = 0
+   
+    @IBAction func touchButton(_ sender: Any) {
+        count = count + 1
+        countLabel.text = "Значение счетчика: \(count)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = "Значение счетчика: 0"
         // Do any additional setup after loading the view.
     }
 
