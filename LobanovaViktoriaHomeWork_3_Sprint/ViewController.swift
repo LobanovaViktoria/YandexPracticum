630741ca60875a298432dc4ebf1f35558372559c
Lobanova Viktoria <va_klevtsova@mail.ru>
2022-11-07 10:43:10 +0300

    Initial Commit

---
+++ LobanovaViktoriaHomeWork_3_Sprint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LobanovaViktoriaHomeWork_3_Sprint
+//
+//  Created by Viktoria Lobanova on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LobanovaViktoriaHomeWork_3_SprintUITests/LobanovaViktoriaHomeWork_3_SprintUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LobanovaViktoriaHomeWork_3_SprintUITestsLaunchTests.swift
+//  LobanovaViktoriaHomeWork_3_SprintUITests
+//
+//  Created by Viktoria Lobanova on 07.11.2022.
+//
+
+import XCTest
+
+final class LobanovaViktoriaHomeWork_3_SprintUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
